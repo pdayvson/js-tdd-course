520f9a9be37e82aedc88f083ce53facc02c26302
P. Dayvson <pdayvson@hotmail.com>
2018-01-16 12:54:40 -0300

    testando push de arquivo com erro no lint

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const a = 3;
+const b = 2
+
+const c = 'sdafasdfasdfasdfasdfasdfasdfasdfasdfasdfafgfghfgjhghjhgjfgjhhjhgjghjghjghjghjfghjghjghjhgg';
+
+const soma = (a1, b1, c1) => a1 + b1 + c1;
+
+soma(a, b, c);
